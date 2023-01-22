9df535d59eb756efccceaf6807f3d84841f6a22a
rismanharism <risman.haris6@gmail.com>
2023-01-22 17:24:55 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,10 @@
+console.log("LOOPING FOR PERTAMA")
+for (i = 1; i <= 20; i++){
+    console.log(i + " - I love coding");
+}
+console.log("LOOPING FOR KEDUA")
+for (i = 20; i >= 1; i--){
+    console.log(i + " - I will become fullstack developer");
+}
+
+    
